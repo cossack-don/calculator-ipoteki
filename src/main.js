e6dbf2e7335a6f2500c18a8de0db7e976ef9add5
cossack-don <ser90593696.zhuravlev@yandex.ru>
2021-06-24 21:31:55 +0300

    добавил роут, для продакшена

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import router from './router'
+import router from './router'
 import store from './store'
 
 
@@ -12,5 +12,6 @@ import "./scss/index.scss";
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
